a7c633fb468b3875d7049055d23802a44896258b
Jason Antman <jason@jasonantman.com>
2019-06-20 12:38:32 -0400

    add errorscan to docker tests

--- dockertest.sh
+++ dockertest.sh
@@ -22,4 +22,4 @@ if [ -z "$1" ]; then
 fi
 
 docker run -it --rm "$1" \
-  bash -c "policygen -V && s3-archiver -V && dryrun-diff -V && manheim-c7n-runner -V && mugc --help"
+  bash -c "policygen -V && s3-archiver -V && dryrun-diff -V && manheim-c7n-runner -V && mugc --help && errorscan -V"
